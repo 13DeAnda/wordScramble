f2a57e98bf0bd974a583b123f9ea528226589eaa
Nora De Anda <nora.e.de.anda@gmail.com>
2015-07-12 15:00:09 -0700

    game working just missing right and wrong answer transitions plus some details

--- scramble.js
+++ scramble.js
@@ -14,25 +14,63 @@ app.directive('scramble', function () {
 
       $scope.shuffledWord = ""; //shuffled word
       $scope.userWord = ""; //apears on left
+      $scope.tansitionColor = "neutral";
       $scope.score = 0;
       $scope.correct = 0;
       $scope.usedLetters = [];
       $scope.timer = 100;
+      $scope.transitionTime = 2;
 
-      $scope.onTimeout = function() {
-        if($scope.timer ===  0) {
-            $timeout.cancel(mytimeout);
-            alert("time is up");
-            return;
+      $scope.getWord = function(){
+        var path = "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=adverb&excludePartOfSpeech=verb&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=8&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+        $http.get(path).then($scope.onGetWord);
+      };
+
+      $scope.onGetWord = function(response){
+        console.log(response.data.word);
+        $scope.tansitionColor = "neutral";
+        $scope.retrivedWord = response.data.word.toLowerCase();
+        $scope.makeWordObject();
+      };
+
+      $scope.makeWordObject = function(){
+        $scope.letters = [];
+        $scope.shuffledWord = $scope.shuffle($scope.retrivedWord);
+
+        for(var i in $scope.shuffledWord){
+          $scope.letters.push({
+            letter : $scope.shuffledWord[i],
+            used : false,
+            color : "unused",
+          });
         }
-        $scope.timer--;
-        mytimeout = $timeout($scope.onTimeout, 1000);
       };
 
       $scope.setTimer = function(){
         $scope.mytimeout = $timeout($scope.onTimeout, 1000);
       };
 
+      $scope.onTimeout = function() {
+        if($scope.timer ===  0) {
+            $timeout.cancel(mytimeout);
+            $scope.userWord = "";
+            $scope.letters = [{}];
+            $scope.message = "";
+            alert("time is up, your score:" +$scope.score);
+            return;
+        }
+        $scope.timer--;
+        mytimeout = $timeout($scope.onTimeout, 1000);
+      };
+
+      $scope.onTransitionTimeout = function(){
+        if($scope.transitionTime ===  0) {
+            $timeout.cancel(transitionTimeout);
+            return;
+        }
+        $scope.transitionTime--;
+        transitionTimeout = $timeout($scope.onTransitionTimeout, 1000);
+      };
       //scrambles a word
       $scope.shuffle = function(string) {
         var parts = string.split('');
@@ -44,8 +82,6 @@ app.directive('scramble', function () {
         }
         return parts.join('');
       };
-
-
       //catches what the user typing
       $scope.type = function(e){
         //if it's a backspace
@@ -62,7 +98,6 @@ app.directive('scramble', function () {
           e.preventDefault();
         }
         else{
-          //if it's not a backspace
           var letter =String.fromCharCode(e.keyCode).toLowerCase();
           var index = $scope.shuffledWord.indexOf(letter);
           if(index > -1){
@@ -71,7 +106,6 @@ app.directive('scramble', function () {
                 $scope.letters[j].used = true;
                 $scope.letters[j].color = "used";
                 $scope.userWord += letter;
-
                 $scope.usedLetters.push($scope.letters[j].letter);
                 $scope.checkWord();
                 break;
@@ -87,6 +121,8 @@ app.directive('scramble', function () {
         if($scope.userWord.length === $scope.retrivedWord.length){
           //same
           if($scope.retrivedWord === $scope.userWord ){
+            $scope.tansitionColor = "correct";
+            $scope.transitionTimeout = $timeout($scope.onTransitionTimeout, 1000);
             $scope.score += 5;
             $scope.correct++;
             $scope.message = "correct";
@@ -96,6 +132,8 @@ app.directive('scramble', function () {
           }
           //anagram
           else if(reverse === $scope.userWord){
+            $scope.tansitionColor = "correct";
+            $scope.transitionTimeout = $timeout($scope.onTransitionTimeout, 1000);
             $scope.score += 5;
             $scope.correct++;
             $scope.message = "correct, it's a anagram of it";
@@ -104,48 +142,24 @@ app.directive('scramble', function () {
           }
           //wrong
           else{
+            $scope.tansitionColor = "incorrect";
             $scope.message = "incorrect";
+            $scope.transitionTimeout = $timeout($scope.onTransitionTimeout, 1000);
             $scope.reset();
           }
         }
       };
 
+      //resets from wrong answer
       $scope.reset = function(){
+        $scope.tansitionColor = "neutral";
         $scope.userWord = "";
         for(var i in $scope.letters){
           $scope.letters[i].color = "unused";
           $scope.letters[i].used = false;
         }
       };
-
-
-      $scope.makeWordObject = function(){
-        $scope.letters = [{}];
-        $scope.shuffledWord = $scope.shuffle($scope.retrivedWord);
-
-        for(var i in $scope.shuffledWord){
-          $scope.letters.push({
-            letter : $scope.shuffledWord[i],
-            used : false,
-            color : "unused",
-          });
-        }
-      };
-
-      $scope.getWord = function(){
-        var path = "http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
-        $http.get(path).then($scope.onGetWord);
-      };
-
-      $scope.onGetWord = function(response){
-        console.log(response.data.word);
-        $scope.retrivedWord = response.data.word;
-        $scope.makeWordObject();
-      }
-
-;
-
-}
+    }
     return {
         restrict: 'E',
         templateUrl:"scramble.html",
